bddf2c120b2f1b8d71c0a6fb767ccf1d5c8a0e48
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-03-19 11:28:26 -0400

    update js

--- js/main.js
+++ js/main.js
@@ -168,4 +168,19 @@
 //     console.log(account.describe())
 // })
 
+/*  
 
+            ASYNC AWAIT HW 
+
+
+*/
+
+
+const fetchPromise = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+
+fetchPromise.then( response => {
+    const jsonPromise = response.json();
+    jsonPromise.then(json => {
+        console.log(json[0].name)
+    });
+});
