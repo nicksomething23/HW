55e49751a9428b0b59f106837c7155e36797e7f3
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-04-30 17:38:51 -0400

    add code

--- js/main.js
+++ js/main.js
@@ -141,3 +141,31 @@
 
 // console.log(aurora.describe());
 // console.log(glacius.describe());
+
+class Account {
+    constructor (name) {
+        this.name = name;
+        this.balance = 0;
+    }
+
+    credit (amount) {
+        this.balance += amount
+    }
+
+    describe () {
+        return `owner: ${this.name}, balance: ${this.balance}`
+    }
+}
+
+const accountList = []
+
+accountList.push(new Account ('Sean'))
+accountList.push(new Account ('Brad'))
+accountList.push(new Account ('Georges'))
+
+accountList.forEach(account => {
+    account.credit(1000)
+    console.log(account.describe())
+})
+
+
